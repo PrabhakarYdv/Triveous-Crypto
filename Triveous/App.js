bedc82f3b309d75af4f88cdd9dc52b2d4ce5e83e
Prabhakar <prabhakarkr802@gmail.com>
2022-01-15 01:37:59 +0530

    Triveous Crypto

---
+++ Triveous/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import type { Node } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+
+
+const App: () => Node = () => {
+  return (
+    <View>
+      <Text>Prabhakar</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
